750dbe0dd6d14e483fe3fe41f76a8aec1e0d7096
Sergey Pavlov <pavlovs@netcracker.com>
2019-10-21 18:20:55 +0200

    'weather'

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import requests
 import telebot
 from telebot.types import Message   
 
@@ -11,11 +12,17 @@ def command_handler(message: Message):
     bot.reply_to(message, 'No answer')
 
 @bot.message_handler(content_types=['text'])
-def echo_digits(message: Message):
-    bot.reply_to(message, str('Хуй'))
+def handle_t(message: Message):
+    if message.text[:7] == "Погода " or message.text[:7] == "погода ":
+            city = message.text[7:]
+            r = requests.get('http://api.openweathermap.org/data/2.5/weather?&units=metric&q=%s&appid=0c9f3c052f1d81b7062750ff0926f345<img src="https://habrastorage.org/files/8fa/5f5/313/8fa5f5313b37438eb250b22cf041f2dd.png" alt="image"/>' % (city))
+            data = r.json()
+            temp = data["main"]["temp"]            
+            bot.send_message(message.chat.id, "Температура в {}: {} C".format(city, temp))
 
 @bot.message_handler(content_types=['sticker'])
 def sticker_handler(message: Message):
     bot.send_sticker(message.chat.id, STICKER_ID)
+
 
 bot.polling(timeout=60)
